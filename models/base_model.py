40f30eb596d3891936e8c6c2b2c79c8b2499ef1e
firacil <firaolmekbib511@gmail.com>
2024-03-08 02:48:11 -0800

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -10,14 +10,30 @@ class BaseModel:
     """The base class for the other class in this package"""
 
     def __init__(self, *args, **kwargs):
-        """Initialization of the class"""
-        self.updated_at = datetime.now()
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+        """
+            Initialization of the class
+
+            Args:
+                args
+                kwargs
+
+        """
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key,
+                            datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                            )
+                elif key != '__class__':
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation of the class"""
-        return ("[{:s}] ({}) {}",
+        return ("[{:s}] ({}) {}"
                 .format(self.__class__.__name__, self.id, self.__dict__)
                 )
 
