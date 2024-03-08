f9c555d27c74f80935a61bc819f33ff98bc5772c
firacil <firaolmekbib511@gmail.com>
2024-03-08 05:34:44 -0800

    console updated with create

--- console.py
+++ console.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
 import cmd
+import os
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """ class HBNBCommand implementing cmd"""
 
     prompt = "(hbnb) "
+
+    def do_create(self, arg):
+        """
+            Creates a new instance of BaseModel,
+            saves it (to the JSON file) and prints the id.
+
+            Args:
+                arg
+        """
+        if not arg or arg is None:
+            print("** class name missing **")
+            return
+
+        if arg not in storage.classes():
+            print("** class doesn't exist **")
+            return
+        else:
+            new_ins = storage.classes()[arg]()
+            new_ins.save()
+            print(new_ins.id)
 
     def do_EOF(self, arg):
         """EOF command to exit the program\n"""
