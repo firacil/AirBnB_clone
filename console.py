38d6d1a484a3e3b5942e8d5ebda48dc1a046d215
firacil <firaolmekbib511@gmail.com>
2024-03-09 09:37:56 -0800

    updated

--- console.py
+++ console.py
@@ -193,6 +193,29 @@ class HBNBCommand(cmd.Cmd):
                     args[0] + '.')]
             print(len(m))
 
+    def update_dict(self, classname, uid, _dict):
+        """Helping method for update() with a dictionary."""
+        s = _dict.replace("'", '"')
+        d = json.loads(s)
+
+        if not classname:
+            print("** class name missing**")
+        elif classname not in storage.classes():
+            print("** class doesn't exist **")
+        elif uid is None:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(classname, uid)
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                attributes = storage.attributes()[classname]
+                for attribute, value in d.items():
+                    if attribute in attributes:
+                        value = attributes[attribute](value)
+                    setattr(storage.all()[key], attribute, value)
+                storge.all()[key].save()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
