ef09c618a274e225db01db8fdd90d14fff4d5b35
firacil <firaolmekbib511@gmail.com>
2024-03-08 23:11:44 -0800

    console_updated

--- console.py
+++ console.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import cmd
-import os
+import re
 from models.base_model import BaseModel
 from models import storage
 
@@ -98,6 +98,51 @@ class HBNBCommand(cmd.Cmd):
             (save the change into the JSON file).
             Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com".
         """
+
+        if args == "" or args is None:
+            print("** class name missing **")
+            return
+
+        reg = r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
+        match = re.search(reg, args)
+        classname = match.group(1)
+        uid = match.group(2)
+        attribute = match.group(3)
+        value = match.group(4)
+
+        if not match:
+            print("** class name missing **")
+        elif classname not in storage.classes():
+            print("** class doesn't exist **")
+        elif uid is None:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(classname, uid)
+            if key not in storage.all():
+                print("** no instance found **")
+            elif not attribute:
+                print("** attribute name missing **")
+            elif not value:
+                print("** value missing **")
+            else:
+                cast = None
+                if not re.search('^".*"$', value):
+                    if '.' in value:
+                        cast = float
+                    else:
+                        cast = int
+                else:
+                    value = value.replace('"', '')
+                    attributes = storage.attributes()[classname]
+                    if attribute in attributes:
+                        value = attributes[attribute](value)
+                    elif cast:
+                        try:
+                            value = cast(value)
+                        except ValueError:
+                            pass
+                    setattr(storage.all()[key], attribute, value)
+                    storage.all()[key].save()
 
     def do_EOF(self, arg):
         """EOF command to exit the program\n"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/pythone3
 """contains the filestorage class"""
+import datetime
 import json
+import os
 
 
 class FileStorage:
@@ -52,3 +54,39 @@ class FileStorage:
                 FileStorage.__objects = objLoaded
         except FileNotFoundError:
             pass
+
+    def attributes(self):
+        """ Returns valid attributes and their type for all classname"""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+                     {"place_id": str,
+                      "user_id": str,
+                      "text": str}
+        }
+        return attributes
