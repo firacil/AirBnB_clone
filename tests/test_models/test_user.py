526d9180e087e6106f2621d35c8e14999eec09d7
Kad-19 <kidusasebe1919@gmail.com>
2024-03-09 12:50:05 -0800

    more user tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -11,23 +11,30 @@ class TestUser(unittest.TestCase):
         """Test if User is subclass of BaseModel"""
         user = User()
         self.assertIsInstance(user, BaseModel)
+        self.assertTrue(hasattr(user, "id"))
+        self.assertTrue(hasattr(user, "created_at"))
+        self.assertTrue(hasattr(user, "updated_at"))
 
     def test_email_attr(self):
         """Tests if User has email attribute"""
         user = User()
         self.assertTrue(hasattr(user, "email"))
+        self.assertEqual(user.email, "")
 
     def test_password_attr(self):
         """Test if user has password attribute"""
         user = User()
         self.assertTrue(hasattr(user, "password"))
+        self.assertEqual(user.password, "")
 
     def test_first_name_attr(self):
         """Test if user has first_name attributes"""
         user = User()
         self.assertTrue(hasattr(user, "first_name"))
+        self.assertEqual(user.first_name, "")
 
     def test_last_name_attr(self):
         """Test if user has last_name attributes"""
         user = User()
         self.assertTrue(hasattr(user, "last_name"))
+        self.assertEqual(user.last_name, "")
