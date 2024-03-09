3de3ed607d269415e53de6a34c5a6c0dc5df4fba
Kad-19 <kidusasebe1919@gmail.com>
2024-03-09 12:36:48 -0800

    Test User

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,7 +2,7 @@
 """Contains the TestUser classes"""
 import unittest
 from models.user import User
-from models.base_mode import BaseModel
+from models.base_model import BaseModel
 
 
 class TestUser(unittest.TestCase):
@@ -11,3 +11,23 @@ class TestUser(unittest.TestCase):
         """Test if User is subclass of BaseModel"""
         user = User()
         self.assertIsInstance(user, BaseModel)
+
+    def test_email_attr(self):
+        """Tests if User has email attribute"""
+        user = User()
+        self.assertTrue(hasattr(user, "email"))
+
+    def test_password_attr(self):
+        """Test if user has password attribute"""
+        user = User()
+        self.assertTrue(hasattr(user, "password"))
+
+    def test_first_name_attr(self):
+        """Test if user has first_name attributes"""
+        user = User()
+        self.assertTrue(hasattr(user, "first_name"))
+
+    def test_last_name_attr(self):
+        """Test if user has last_name attributes"""
+        user = User()
+        self.assertTrue(hasattr(user, "last_name"))
