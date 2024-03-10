8aac3c7d0c4f55033b185be032fb6a59fad32d10
firacil <firaolmekbib511@gmail.com>
2024-03-10 00:11:11 -0800

    test_updated

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+""" Unittesting base_model module"""
+from models import storage
+import unittest
+import uuid
+import json
+import os
+import time
+from models.engine.file_storage import FileStorage
+from datetime import datetime
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.Testcase):
+    """ Testing the User Class"""
+    
+    def setUp(self):
+        """ sets up test method"""
+        pass
+
+    def no_args_init_(self):
+        """ Test when __init__ with no arguments"""
+        self.Storagereset()
+        with self.assertRaises(TypeError) as e:
+            BaseModel.__init__()
+            m = "__init__() missing 1 required positional argument: 'self'"
+            self.assertEqual(str(e.exception), m)
+
+    def test_id(self):
+        """Test for unique uuid"""
+        u = [BaseModel().id for i in range(100)]
+        self.assertEqual(len(set(u)), len(u))
+
+    def test_str(self):
+        """ test for __str__ method"""
+        bm = BaseModel()
+        reg = re.compile(r"^\[(.*)\] \((.*)\) (.*)$")
+        res = reg.match(str(bm))
+        self.assertISNotNone(res)
+        self.assertEqual(res.group(1), "BaseModel")
+        self.assertEqual(res.group(2), bm.id)
+        s = res.group(3)
+        s = re.sub(r"(datetime\.datetime\([^)]*))", "'\\1'", s)
+        d = json.loads(s.replace("'", '"'))
+        dd = bm.__dict__.copy()
+        dd["created_at"] = repr(dd["created_at"])
+        dd["updated_at"] = repr(dd["updated_at"])
+        self.assertEqual(d, dd)
+
+    def test_save(self):
+        """ tests method save()"""
+        bm = BaseModel()
+        date_now = datetime.now()
+        time.sleep(0.5)
+        bm.save()
+        d = bm.updated_at - date_now
+        self.assertTrue(abs(d.total_seconds()) < 0.01)
+
+    def test_to_dict(self):
+        """ Tests to_dict method"""
+        bm = BaseModel()
+        bm.name = "John"
+        bm.age = 89
+        dic = bm.to_dict()
+        self.assertEqual(dic["id"], bm.id)
+        self.assertEqual(dic["__class__"], type(bm).__name__)
+        self.assertEqual(dic["created_at"], bm.created_at.isoformat())
+        self.assertEqual(dic["updated_at"], bm.updated_at.isoformat())
+        self.assertEqual(dic["name"], bm.name)
+        self.assertEqual(dic["age"], bm.age)
+
+    def Storagereset(self):
+        """ Resets Filestorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+
+if __name__ == '__main__':
+    unittest.main()
